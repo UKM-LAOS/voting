eb47bf8abb68625c4a96b9632c1bf0208bfff9f9
Hilman F. Rakhmad <hifraofficial@gmail.com>
2021-12-04 12:27:43 +0700

    fix db seed and blade

--- database/seeders/StatusVerificationSeeder.php
+++ database/seeders/StatusVerificationSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -16,22 +17,30 @@ class StatusVerificationSeeder extends Seeder
     {
         DB::table('status_verifications')->insert([
             'id' => 0,
-            'title' => "Belum mengisi form"
+            'title' => "Belum mengisi form",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
 
         DB::table('status_verifications')->insert([
             'id' => 1,
-            'title' => "Menunggu verifikasi admin"
+            'title' => "Menunggu verifikasi admin",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
 
         DB::table('status_verifications')->insert([
             'id' => 2,
-            'title' => "Telah terverifikasi"
+            'title' => "Telah terverifikasi",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
 
         DB::table('status_verifications')->insert([
             'id' => 3,
-            'title' => "Ada kesalahan pada berkas anda"
+            'title' => "Ada kesalahan pada berkas anda",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -19,11 +20,14 @@ class UserSeeder extends Seeder
             'email' => 'admin@example.com',
             'role' => 1,
             'password' => '$2y$10$VeoEoOdEfeulqcyDOqnm/esM678riBjnjenhgOfA12wY0VpjMHS5S', // 'password' without quote, change ASAP.
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
 
         DB::table('user_verifications')->insert([
             'user_id'=>$id,
-
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
     }
 }
